c8873109277ae0dc59df609627fc535ed55a84f5
ssantoshaws2 <ssantoshaws2@gmail.com>
2024-08-25 18:57:10 +0530

    ss

--- subnet-associate.tf
+++ subnet-associate.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.tf_public_subnet.id
+  subnet_id      = aws_subnet.ss_public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
