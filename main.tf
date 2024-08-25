c67f38a783800f54b49953f3f71110c07f309f90
ssantoshaws2 <ssantoshaws2@gmail.com>
2024-08-25 17:28:39 +0530

    25

--- main.tf
+++ main.tf
@@ -3,7 +3,15 @@ provider "aws" {
 
 }
 
-resource "aws_instance" "foo" {
+resource "aws_vpc" "main2" {
+  cidr_block = "200.0.0.0/16"
+
+  tags = {
+    Name = "SS VPC"
+  }
+}
+
+resource "aws_instance" "main2" {
   ami           = "ami-022ce6f32988af5fa" 
   instance_type = "t2.micro"
   tags = {
--- vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "main2" {
-  cidr_block = "200.0.0.0/16"
-
-  tags = {
-    Name = "SS VPC"
-  }
-}
