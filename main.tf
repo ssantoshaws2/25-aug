f386df16e8dfc1a390c3fd68a1247edc3e2dc668
ssantoshaws2 <ssantoshaws2@gmail.com>
2024-08-25 18:01:20 +0530

    ss

--- main.tf
+++ main.tf
@@ -4,19 +4,12 @@ provider "aws" {
 }
 
 
-resource "aws_vpc" "main2" {
-  cidr_block = "200.0.0.0/16"
-
-  tags = {
-    Name = "SS VPC"
-  }
-}
 
 
 resource "aws_instance" "main2" {
   ami           = "ami-022ce6f32988af5fa" 
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance jenking 26 aug pvt"
+      Name = "TF-Instance jenking 26 aug"
   }
 }
