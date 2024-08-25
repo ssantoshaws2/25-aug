6acdc807dd00b62ec8aa9cfb21c6170c2cc0104e
ssantoshaws2 <ssantoshaws2@gmail.com>
2024-08-25 18:14:18 +0530

    ss

---
+++ rt.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main2.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.some_ig.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.some_ig.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
