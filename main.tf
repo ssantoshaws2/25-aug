226357f32c1a649ecb014295601d121ba3bd77f6
ssantoshaws2 <ssantoshaws2@gmail.com>
2024-08-26 17:10:08 +0530

    ss

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "web_instanc3" {
   associate_public_ip_address = "true"
  vpc_security_group_ids      = [aws_security_group.web_sg.id]
   tags = {
-  Name = "web-server3" 
+  Name = "web-server01" 
     }
 user_data =  <<-EOF
  #!/bin/bash
